2183fedee9f5b71eee1c7edbe01e0db362e0efc9
Ali Sarawan <ali.sarawan13@gmail.com>
2022-11-24 23:25:23 +0200

    Insertion, Deletion,and search methods LinkedList class.

--- LinkedList.java
+++ LinkedList.java
@@ -3,7 +3,7 @@ public class LinkedList<T> {
     public boolean isEmpty(){
         return first==null;
     }
-    public void insert(T data){
+    public void insertAtBack(T data){
         Node<T> n=new Node(data);
         if(isEmpty()){
             first=n;
@@ -16,5 +16,67 @@ public class LinkedList<T> {
             current.next=n;
         }
     }
-//test comment
+    public void insertAtFront(T data){
+        Node<T> n=new Node(data);
+        if(isEmpty())
+            first=n;
+        else{
+            n.next=first;
+            first=n;
+        }
+    }
+    public T deleteFromFront(){
+        if(isEmpty()){
+            return null;
+        }
+        else{
+            T data= first.data;
+            first=first.next;
+            return data;
+        }
+    }
+    public T deleteFromBack(){
+        if(isEmpty()){
+            return null;
+        }
+        else if(first.next==null){
+            T data= first.data;
+            first=null;
+            return data;
+        }
+        else{
+            Node<T> current=first;
+            while(current.next.next!=null){
+                current=current.next;
+            }
+            T data=current.next.data;
+            current.next=null;
+            return data;
+        }
+    }
+    public boolean search(T data) {
+        if (!isEmpty()) {
+            Node<T> current = first;
+            while (current != null) {
+                if (current.data.equals(data))
+                    return true;
+                current = current.next;
+            }
+        }
+        return false;
+    }
+    public boolean searchAndDelete(T data) {
+        if (!isEmpty()) {
+            Node<T> current = first, previous = null;
+            while (current != null) {
+                if (current.data.equals(data)) {
+                    previous.next = current.next;
+                    return true;
+                }
+                previous = current;
+                current = current.next;
+            }
+        }
+        return false;
+    }
 }
